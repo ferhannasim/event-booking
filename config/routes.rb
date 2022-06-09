d44bdb60bf761e37f7353dab15eed9c5129d58f5
ferhan_nasim <ferhannasim@gmail.com>
2022-06-09 19:34:03 +0500

    Added routes, webpacker files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :tours
+  resources :operators
+
+  get '/get_tour_types' => 'tours#get_tour_types'
 end
